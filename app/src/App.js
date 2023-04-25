79c57ef6f9330cf09f4ac1e7494440c97708b61f
Crostons <shubh1608003@gmail.com>
2023-04-25 15:38:32 +0530

    React and Tailwind installation

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-teal-300">
+      hello World
+    </div>
+  );
+}
+
+export default App;
